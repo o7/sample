d5bd08eeb3c49cdd571b4113ea735bf3a92617dc
lessless <yevhenii.kurtov@gmail.com>
2019-06-07 22:54:37 +0200

    Start n2o from application supervisor

--- lib/application.ex
+++ lib/application.ex
@@ -1,12 +1,28 @@
 defmodule Sample.Application do
   use Application
+
   def start(_type, _args) do
-      {:tls, :cowboy.start_tls(:http,
-           [ port: Application.get_env(:n2o, :port, 8001),
-             certfile:   :code.priv_dir(:sample) ++ '/ssl/fullchain.pem',
-             keyfile:    :code.priv_dir(:sample) ++ '/ssl/privkey.pem',
-             cacertfile: :code.priv_dir(:sample) ++ '/ssl/fullchain.pem' ],
-          %{ env: %{dispatch: :n2o_cowboy2.points()}} )} |> IO.inspect
-      Sample.Supervisor.start_link([name: :sample])
+    start_cowboy()
+    :n2o.start([], [])
+
+    children = []
+
+    opts = [strategy: :one_for_one, name: Tchk.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp start_cowboy() do
+    {:tls,
+     :cowboy.start_tls(
+       :http,
+       [
+         port: Application.get_env(:n2o, :port, 8001),
+         certfile: :code.priv_dir(:sample) ++ '/ssl/fullchain.pem',
+         keyfile: :code.priv_dir(:sample) ++ '/ssl/privkey.pem',
+         cacertfile: :code.priv_dir(:sample) ++ '/ssl/fullchain.pem'
+       ],
+       %{env: %{dispatch: :n2o_cowboy2.points()}}
+     )}
+    |> IO.inspect()
   end
 end
--- mix.exs
+++ mix.exs
@@ -1,10 +1,14 @@
 defmodule Sample.Mixfile do
   use Mix.Project
-  def project, do: [ app: :sample, version: "6.6.6", elixir: "~> 1.7", deps: deps() ]
-  def application, do: [ mod: { Sample.Application, [] }, applications: [:cowboy, :n2o, :gproc, :kvx, :logger] ]
-  def deps, do: [ {:n2o,   github: "synrc/n2o"},
-                  {:gproc, github: "voxoz/gproc"},
-                  {:kvx,   github: "synrc/kvx"},
-                  {:nitro, github: "synrc/nitro"},
-                  {:cowboy, "~> 2.5"} ]
+  def project, do: [app: :sample, version: "6.6.6", elixir: "~> 1.7", deps: deps()]
+  def application, do: [mod: {Sample.Application, []}]
+
+  def deps,
+    do: [
+      {:n2o, github: "synrc/n2o"},
+      {:gproc, github: "voxoz/gproc"},
+      {:kvx, github: "synrc/kvx"},
+      {:nitro, github: "synrc/nitro"},
+      {:cowboy, "~> 2.5"}
+    ]
 end
