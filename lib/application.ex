880b50f5d0164b28032f3b8fd661e848bb33307a
Namdak Tonpa <maxim@synrc.com>
2019-06-10 10:19:19 +0300

    go fmt

--- lib/application.ex
+++ lib/application.ex
@@ -1,28 +1,16 @@
 defmodule Sample.Application do
   use Application
-
-  def start(_type, _args) do
+  def start(_,_) do
     start_cowboy()
     :n2o.start([], [])
-
-    children = []
-
-    opts = [strategy: :one_for_one, name: Sample.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link([], [strategy: :one_for_one, name: Sample.Supervisor])
   end
-
   defp start_cowboy() do
-    {:tls,
-     :cowboy.start_tls(
-       :http,
-       [
-         port: Application.get_env(:n2o, :port, 8001),
+    {:tls, :cowboy.start_tls(:http,
+       [ port: Application.get_env(:n2o, :port, 8001),
          certfile: :code.priv_dir(:sample) ++ '/ssl/fullchain.pem',
          keyfile: :code.priv_dir(:sample) ++ '/ssl/privkey.pem',
-         cacertfile: :code.priv_dir(:sample) ++ '/ssl/fullchain.pem'
-       ],
-       %{env: %{dispatch: :n2o_cowboy2.points()}}
-     )}
-    |> IO.inspect()
+         cacertfile: :code.priv_dir(:sample) ++ '/ssl/fullchain.pem' ],
+      %{ env: %{dispatch: :n2o_cowboy2.points()} })} |> IO.inspect()
   end
 end
--- mix.exs
+++ mix.exs
@@ -1,14 +1,13 @@
 defmodule Sample.Mixfile do
   use Mix.Project
   def project, do: [app: :sample, version: "6.6.6", elixir: "~> 1.7", deps: deps()]
-  def application, do: [mod: {Sample.Application, []}]
-
+  def application, do: [extra_applications: [:cowboy, :n2o], mod: {Sample.Application, []}]
   def deps,
     do: [
-      {:n2o, github: "synrc/n2o"},
-      {:gproc, github: "voxoz/gproc"},
-      {:kvx, github: "synrc/kvx"},
+      {:n2o,   github: "synrc/n2o"},
+      {:kvx,   github: "synrc/kvx"},
       {:nitro, github: "synrc/nitro"},
+      {:gproc, github: "voxoz/gproc"},
       {:cowboy, "~> 2.5"}
     ]
 end
