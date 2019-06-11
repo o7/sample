9c05b3c089952be0ff22a3a177332af3f14c9f97
dima <dmitrii.bushmelev@gmail.com>
2019-06-11 19:19:56 +0200

    Apply mix format and some style changes

--- config/config.exs
+++ config/config.exs
@@ -4,11 +4,10 @@ config :n2o,
   event: :pickle,
   formatter: :n2o_bert,
   pickler: :n2o_secret,
-  protocols: [:n2o_nitro,:n2o_ftp],
+  protocols: [:n2o_nitro, :n2o_ftp],
   routes: Sample.Routes
 
 config :kvx,
   dba: :kvx_rocks,
   dba_st: :kvx_st,
-  schema: [:kvx,:kvx_stream]
-
+  schema: [:kvx, :kvx_stream]
